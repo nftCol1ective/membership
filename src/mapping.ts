83dec61101a950a656eca6c353bd3cdea1e03cb9
Michael Vogt <info@michaelvogt.eu>
2022-03-22 07:27:44 +0100

    Create new entities

--- src/mapping.ts
+++ src/mapping.ts
@@ -10,12 +10,12 @@ import { MembershipEntity } from "../generated/schema"
 export function handleMembershipCreated(event: MembershipCreated): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = MembershipEntity.load(event.transaction.from.toHex())
+  let entity = MembershipEntity.load(event.params.tokenId.toHex())
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
   if (!entity) {
-    entity = new MembershipEntity(event.transaction.from.toHex())
+    entity = new MembershipEntity(event.params.tokenId.toHex())
   }
 
   // Entity fields can be set based on event parameters
